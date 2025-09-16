fccb85b1894fab8d0a5e13ad160b91cb1b0073da
fardanaljihad <fardanaljihad.18@gmail.com>
2025-09-16 17:34:07 +0700

    Add get user endpoint

--- src/application/web.js
+++ src/application/web.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { publicRuoter } from "../routes/public-api.js";
 import { errorMiddleware } from "../middlewares/error-middleware.js";
+import { userRouter } from "../routes/api.js";
 
 export const web = express();
 web.use(express.json());
 
 web.use(publicRuoter);
+web.use(userRouter)
 
 web.use(errorMiddleware);
